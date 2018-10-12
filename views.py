fc21d2214015dff26c08458b438b1b5e200c8795
cWjL <jacobloden@hotmail.com>
2018-10-11 21:32:59 -0700

    fixed arguments

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import colorama
 from colorama import Fore, Style
-import argparse,sys
+import argparse,sys,time
 from src.mask import mask
 
 def main():
@@ -9,23 +9,21 @@ def main():
     g_prefix = "["+Fore.GREEN+"*"+Style.RESET_ALL+"] "
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t",action='store',dest='tor_path',help='Path to start_tor_browser executable')
     parser.add_argument("-v", action='store',dest='visit_url',help='View this page a whole bunch of times')
     parser.add_argument("-p",action='store',dest='vote_url',help='Vote in this poll')
     parser.add_argument("-N",action='store',dest='num',help='Do it this many times')
 
     args = parser.parse_args()
-    tor = None
-    if not args.tor_path:
-        print(b_prefix+"You must enter the file path to your start_tor_browser executable")
-        print(b_prefix+"Try again")
-        sys.exit(1)
-    elif not args.num:
+    if not args.num:
         print(b_prefix+"You must enter the number of visits/votes you want")
         print(b_prefix+"Try again")
         sys.exit(1)
     elif args.visit_url and args.vote_url:
         print(b_prefix+"You can either vote or visit, not both")
+        print(b_prefix+"Try again")
+        sys.exit(1)
+    elif not args.visit_url and not args.vote_url:
+        print(b_prefix+"You must either vote or visit")
         print(b_prefix+"Try again")
         sys.exit(1)
     elif args.visit_url:
@@ -34,15 +32,33 @@ def main():
         url_to_vote = args.vote_url
     num = args.num
 
+    # Get tor browser.  Need to pass this off to the specific option, either views or votes
+    # The following os for testing only
     try:
-        tor = mask(args.tor_path)
+        print(g_prefix+"Starting TOR services")
+        tor = mask(get_config())
+        print(g_prefix+"Starting TOR browser")
+        tor._start_tor()
+        time.sleep(5)
     except Exception as e:
         print(b_prefix+"Error: "+str(e))
         sys.exit(1)
 
-    print(g_prefix+"TEST")
+    #print(g_prefix+"TEST")
     
     sys.exit(0)
+
+def get_config():
+    conf_lines = []
+    with open('src/views.conf', 'r') as conf:
+        all_conf_lines = conf.readlines()
+        for line in all_conf_lines:
+            if '#' not in line:
+                conf = line.split(' ')
+                conf_lines.append(conf[1].replace('\n', ''))
+    return(conf_lines[0])
+
+    
 
 if __name__ == "__main__":
     colorama.init()
