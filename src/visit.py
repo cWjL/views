04c8121f012933f9bf38707df2d0865f7cb093b5
cWjL <jacobloden@hotmail.com>
2018-10-13 14:04:56 -0700

    added test loop

--- src/visit.py
+++ src/visit.py
@@ -1,8 +1,17 @@
-import stem.process
+import stem.process, time
 from stem import Signal
 from stem.control import Controller
 
 class visit():
     def __init__(self, tor, url):
-        self.tor = tor
+        self.tor_driver = tor
         self.url = url
+        self._run()
+
+    def _run(self):
+        for i in range(5):
+            browser = self.tor_driver.get_tor_browser()
+            browser.get(self.url)
+            time.sleep(2)
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
