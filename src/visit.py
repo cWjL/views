aea8ea274e0e95ead8ccefe00c3149585e1b7e9b
cWjL <jacobloden@hotmail.com>
2018-10-15 18:32:54 -0700

    added ip swap

--- src/visit.py
+++ src/visit.py
@@ -1,17 +1,28 @@
 import stem.process, time
 from stem import Signal
 from stem.control import Controller
+from progressbar import ProgressBar
 
 class visit():
-    def __init__(self, tor, url):
+    def __init__(self, tor, url, n):
         self.tor_driver = tor
         self.url = url
+        self.n = int(n)
         self._run()
 
     def _run(self):
-        for i in range(5):
+        bar = ProgressBar()
+        for i in bar(range(self.n)):
             browser = self.tor_driver.get_tor_browser()
-            browser.get(self.url)
-            time.sleep(2)
-            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)
+            if browser is not None:
+                browser.get(self.url)
+                time.sleep(2)
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(2)
+                if self.tor_driver.swap_ident():
+                    #TODO log ip swap
+                else:
+                    #TODO log ip swap failure
+            else:
+                return False
+        return True
