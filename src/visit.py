e9870517e733b89a04e2e1356947ec14b2dc2acc
cWjL <jacobloden@hotmail.com>
2018-10-15 19:43:09 -0700

    fixed return

--- src/visit.py
+++ src/visit.py
@@ -2,6 +2,7 @@ import stem.process, time
 from stem import Signal
 from stem.control import Controller
 from progressbar import ProgressBar
+from random import randint
 
 class visit():
     def __init__(self, tor, url, n):
@@ -16,13 +17,15 @@ class visit():
             browser = self.tor_driver.get_tor_browser()
             if browser is not None:
                 browser.get(self.url)
-                time.sleep(2)
                 browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(2)
+                browser.close() # Close browser
+                time.sleep(randint(0,5)) # Sleep random time 
                 if self.tor_driver.swap_ident():
                     #TODO log ip swap
+                    tmp = ""
                 else:
                     #TODO log ip swap failure
+                    tmp = ""
             else:
                 return False
         return True
