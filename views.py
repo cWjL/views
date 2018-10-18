f2d8467accc7ecca0b2a55f317ef42a108f094be
cWjL <jacobloden@hotmail.com>
2018-10-17 21:21:47 -0700

    added file path check

--- views.py
+++ views.py
@@ -4,14 +4,16 @@ from colorama import Fore, Style
 import argparse,sys,time,os,logging
 from src.visits import visits
 
-def main():
+def main(log_path):
     b_prefix = "["+Fore.RED+"*"+Style.RESET_ALL+"] "
     g_prefix = "["+Fore.GREEN+"*"+Style.RESET_ALL+"] "
-
+    
     log = logging.getLogger(__name__)
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%a, %d %b %Y %H:%M:%S', filename='logs/sec.log', filemode='w')
 
+    log_path = ck_path(log_path)
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", action='store',dest='visit_url',help='View this page a whole bunch of times')
     parser.add_argument("-p",action='store',dest='vote_url',help='Vote in this poll')
@@ -61,6 +63,16 @@ def main():
     print(g_prefix+"Exiting")
     sys.exit(0)
 
+def ck_path(fp):
+    if fp[len(fp)-1] == "\\" or fp[len(fp)-1] == "/":
+        return fp
+    else:
+        if "\\" in fp:
+            fp = fp + "\\"
+        else:
+            fp = fp + "/"
+    return fp
+
 def get_config():
     conf_lines = []
     with open('src/views.conf', 'r') as conf:
@@ -75,4 +87,4 @@ if __name__ == "__main__":
     colorama.init()
     if not os.path.exists("logs"):
         os.makedirs("logs")
-    main()
+    main(os.path.abspath("logs"))
