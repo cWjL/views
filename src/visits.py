e9ad7fcaae8dbd4e22480bc63261f459f07d285e
cWjL <jacobloden@hotmail.com>
2018-10-18 18:07:34 -0700

    added vote stuff

--- src/visits.py
+++ src/visits.py
@@ -20,16 +20,33 @@ class visits():
         if self.visit is not None:
             from src.visit import visit
             print(g_prefix+"Starting site visits")
-            if visit(tor, self.visit, self.num, self.log):
+            vis = visit(tor, self.visit, self.num, self.log)
+            if vis.run():
                 print(g_prefix+"Site visited "+self.num+" times, with "+self.num+" different IP addresses")
             else:
                 print(b_prefix+"Something went wrong, check log for details")
         else:
             from src.vote import vote
-            # Do vote stuff
-            # Need to pass it the tor object
+            
+            print(g_prefix+"Starting to vote")
+            vot = vote(tor, self.vote, self.num, vote_id, self.log)
+            if vot.run():
+                print(g_prefix+"Voted "+self.num+" times, with "+self.num+" differernt IP addresses")
+            else:
+                print(b_prefix+"Something went worng, check log for details")
 
-
+    def _how_to_id(self):
+        print(g_prefix+"You need to provide the \"ID\" of the element you want to vote.")
+        print(g_prefix+"Navigate to "+self.url)
+        print(g_prefix+"Find the voting item (button, option, etc.), right click on it and click \"Inspect Element\".")
+        print(g_prefix+"Locate the id, it'll look something like the examples below:")
+        print("\t\t<p class=\"row answer-text flex-auto flex-center ng-binding\">DO IT</p>")
+        print("\t\t\tIn this case, the \"ID\" would be \"DO IT\"")
+        print("\t\t<input name=\"options\" value=\"136886764\" id=\"field-options-one\" type=\"radio\">")
+        print("\t\t\tIn this case, the \"ID\" would be \"field-option-one\"")
+        print("<input name=\"options\" value=\"136886765\" id=\"field-options-two\" type=\"radio\">")
+        print("\t\t\tIn this case, the \"ID\" would be \"field-option-two\"")
+#
     def _get_config(self):
         # Need to add file path format check.  Needs to end with a '\'
         # Maybe push config import to a seperate object
